39d4ee345d788e71cf4adf7a820c92e763f916a8
DouglasVargas123 <d.vargasvsquez1@uandresbello.edu>
2022-12-04 19:59:39 -0300

    Cambio51

--- TallerDePrototipo/Assets/Todo/Scripts/ScriptsNivel/Jefe/MatarJefe.cs
+++ TallerDePrototipo/Assets/Todo/Scripts/ScriptsNivel/Jefe/MatarJefe.cs
@@ -2,28 +2,50 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
 
 public class MatarJefe : MonoBehaviour
 {
     public GameObject Enemigo; //Object del enemigo
     public bool EnemigoVivo = true; //El jugador empieza vivo
-    public float vida = 10;
-    public float vidaMaxima = 10;
+    public float vida = 9;
+    public float vidaMaxima = 9;
 
     public BoxCollider BXArriba; //Box colider del enemigo
-    public int BossMuerto = 0;
+    public bool BossMuerto = false;
 
     public Image BarraVida;
 
+    public float Cuenta = 0;
+    public int Escena;
+    public Animator anim;
 
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
 
     private void Update()
     {
         BarraVida.fillAmount = vida / vidaMaxima;
         if (vida == 0) //Si el enemigo muere
         {
-            Enemigo.SetActive(false); //El enemigo desaparece
+            BossMuerto = true; //El enemigo desaparece
             BXArriba.enabled = false; //Desactiva el box colider para que no mande al jugador al lobby
+        }
+        if(BossMuerto)
+        {
+            Cuenta = Cuenta + 1 * Time.deltaTime;
+            anim.SetBool("Muere", true);
+
+        }
+        if(Cuenta >= 5)
+        {
+            Enemigo.SetActive(false);
+            SceneManager.LoadScene(Escena);
+            
         }
 
         
